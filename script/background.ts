02abb37b0f792fd568e7a26046ece02fc8546983
Flocon <flocon@localhost.localdomain>
2020-11-17 01:15:20 +0100

    Cleaned code, fixed pages for which view-source is inactivated

--- script/background.ts
+++ script/background.ts
@@ -1,3 +1,7 @@
+/************************************************************************************/
+// Global variables definition
+/************************************************************************************/
+
 // @ts-ignore
 let browser = browser;
 
@@ -8,17 +12,23 @@ let currentUrl: string = '';
 
 const prefix: string = "view-source:";
 
+/************************************************************************************/
+// Functions definition
+/************************************************************************************/
+
+// Function tied to the browser action button.
+// Retrieve URL and tab id of the active tab
+// Add or remove a "view-source:" prefix to
+// this URL.
 async function switchUrl() {
 
     // @ts-ignore
     let currentTab;
-    // let currentUrl: string = ''; TODO remove
     let currentId: number = 0;
-    let newUrl: string = "about:newtab";
+    let newUrl: string = "";
 
     try {
         let tabs = await browser.tabs.query({ currentWindow: true, active: true });
-
         currentTab = tabs[0];
         currentUrl = currentTab.url;
         currentId = currentTab.id;
@@ -36,58 +46,47 @@ async function switchUrl() {
         await browser.tabs.update(currentId, { active: true, url: newUrl });
     } catch (err) {
         console.log("View Source Switcher: Could not update the current tab to " + newUrl + "\n", err);
+    } finally {
+        setCurrentUrlAndIcon();
     }
-
-    if (newUrl.startsWith(prefix)) {
-        browser.browserAction.setIcon({ path: "icons/icon_on.svg" });
-    } else {
-        browser.browserAction.setIcon({ path: "icons/icon.svg" });
-    }
-
 }
 
-browser.browserAction.onClicked.addListener(switchUrl);
-
-
+// Function that set browser action button
+// icon depending on the URL in paramater.
 function setBrowserIcon(url: string) {
-
     if (url.startsWith(prefix)) {
         browser.browserAction.setIcon({ path: "icons/icon_on.svg" });
     } else {
         browser.browserAction.setIcon({ path: "icons/icon.svg" });
     }
-
 }
 
+// Function called back when a tab is updated
 function handleUpdated(tabId: number, changeInfo: any, tabInfo: any) {
     if (changeInfo.url) {
-        console.log("Tab: " + tabId +
-            " URL changed to " + changeInfo.url);
         setBrowserIcon(changeInfo.url);
     }
 }
 
-browser.tabs.onUpdated.addListener(handleUpdated);
-
+// Function that update the currentUrl global
+// variable and set the browser action button
+// accordingly.
 async function setCurrentUrlAndIcon() {
-
     try {
         let tabs = await browser.tabs.query({ currentWindow: true, active: true });
-
         let currentTab = await tabs[0];
         currentUrl = await currentTab.url;
-
-        console.log(currentUrl);
     } catch (err) {
         console.log("View Source Switcher: Could not retrieve the current tab...\n", err);
     }
 
-    if (currentUrl.startsWith(prefix)) {
-        browser.browserAction.setIcon({ path: "icons/icon_on.svg" });
-    } else {
-        browser.browserAction.setIcon({ path: "icons/icon.svg" });
-    }
-
+    setBrowserIcon(currentUrl);
 }
 
+/************************************************************************************/
+// Listeners definition
+/************************************************************************************/
+
 browser.tabs.onActivated.addListener(setCurrentUrlAndIcon);
+browser.tabs.onUpdated.addListener(handleUpdated);
+browser.browserAction.onClicked.addListener(switchUrl);
